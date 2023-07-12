948ba1060aaf90b59e3d1ccdc3fe2548a3176eb4
Nombre prueba <prueba@ejemplo.com>
2023-07-12 19:35:10 +0200

    intento anadit sandbox a Modulo 2

---
+++ Modulo2/main.ts
@@ -0,0 +1,7 @@
+import "./style.css";
+
+console.log(
+  "Ticket cena valor 120, puedo pagar solo 18 euros de bebidas, entonces debemos dividir entre los 6 la diferencia lo cual nos da lo que cada uno pagara y es:"
+);
+
+console.log((120 - 8) / 6);
